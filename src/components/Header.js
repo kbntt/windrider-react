f84c56c4893e9f6e9fb19e7baf0c52eb47f788c8
windRiderLee <kbntt73@gmail.com>
2022-03-10 17:37:24 +0900

    Routes 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,17 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import { Routes, Route } from 'react-router-dom';
+import Home from './../pages/Home';
+import Design from './../pages/Design';
+import Culture from './../pages/Culture';
+import Business from './../pages/Business';
+import Politics from './../pages/Politics';
+import Opinion from './../pages/Opinion';
+import Science from './../pages/Science';
+import Health from './../pages/Health';
+import Style from './../pages/Style';
+import Travel from './../pages/Travel';
 
 function Header(props) {
   const { sections, title } = props;
@@ -49,6 +60,18 @@ function Header(props) {
           </Link>
         ))}
       </Toolbar>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/design" element={<Design />}></Route>
+        <Route path="/culture" element={<Culture />}></Route>
+        <Route path="/business" element={<Business />}></Route>
+        <Route path="/politics" element={<Politics />}></Route>
+        <Route path="/opinion" element={<Opinion />}></Route>
+        <Route path="/science" element={<Science />}></Route>
+        <Route path="/health" element={<Health />}></Route>
+        <Route path="/style" element={<Style />}></Route>
+        <Route path="/travel" element={<Travel />}></Route>
+      </Routes>
     </React.Fragment>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import MainFeaturedPost from './../components/MainFeaturedPost';
+import FeaturedPost from './../components/FeaturedPost';
+import Grid from '@mui/material/Grid';
+
+const Home = () => {
+  const mainFeaturedPost = {
+    title: 'Title of a longer featured blog post',
+    description:
+      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    image: 'https://source.unsplash.com/random',
+    imageText: 'main image description',
+    linkText: 'Continue reading…',
+  };
+
+  const featuredPosts = [
+    {
+      title: 'Featured post',
+      date: 'Nov 12',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random',
+      imageLabel: 'Image Text',
+    },
+    {
+      title: 'Post title',
+      date: 'Nov 11',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random',
+      imageLabel: 'Image Text',
+    },
+  ];
+
+  return (
+    <div>
+      <main>
+        <MainFeaturedPost post={mainFeaturedPost} />
+        <Grid container spacing={4}>
+          {featuredPosts.map((post) => (
+            <FeaturedPost key={post.title} post={post} />
+          ))}
+        </Grid>
+        <Grid container spacing={5} sx={{ mt: 3 }}></Grid>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
